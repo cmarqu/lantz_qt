8000b134caf2ccf36ac3954872a1deb052af7250
Hernan Grecco <hernan.grecco@gmail.com>
2015-02-08 22:53:43 -0300

    Extracted Qt from Lantz

--- setup.py
+++ setup.py
@@ -30,23 +30,7 @@ if sys.version_info < (3, 4):
 
 root_folder = os.path.dirname(os.path.abspath(__file__))
 
-# Compile a list of companies with drivers.
-folder = os.path.join(root_folder, 'lantz', 'drivers')
-paths = os.listdir(folder)
-companies = [path for path in paths
-             if os.path.isdir(os.path.join(folder, path))
-             and os.path.exists(os.path.join(folder, path, '__init__.py'))]
-
-
-# Compile a list of companies with legacy drivers.
-folder = os.path.join(root_folder, 'lantz', 'drivers', 'legacy')
-paths = os.listdir(folder)
-legacy_companies = [path for path in paths
-                    if os.path.isdir(os.path.join(folder, path))
-                    and os.path.exists(os.path.join(folder, path, '__init__.py'))]
-
-
-setup(name='Lantz',
+setup(name='Lantz-qt',
       version='0.4.dev0',
       license='BSD',
       description='Instrumentation framework',
@@ -55,29 +39,18 @@ setup(name='Lantz',
       author='Hernan E. Grecco',
       author_email='hernan.grecco@gmail.com',
       url='http://lantz.glugcen.dc.uba.ar/',
-      packages=['lantz',
-                'lantz.ui',
-                'lantz.ui.blocks',
-                'lantz.simulators',
-                'lantz.utils',
-                'lantz.drivers'] +
-               ['lantz.drivers.' + company for company in companies] +
-               ['lantz.drivers.legacy.' + company for company in legacy_companies],
-      test_suite='lantz.testsuite.testsuite',
-      install_requires=['pint>=0.6',
-                        'pyvisa>=1.6.2',
-                        'stringparser',
-                       ] + requirements,
+      packages=['lantz_qt',
+                'lantz_qt.ui',
+                'lantz_qt.ui.blocks',
+                'lantz_qt.utils'],
+      install_requires=['lantz_core>=0.4'],
       zip_safe=False,
       platforms='any',
       entry_points={
            'zest.releaser.releaser.after_checkout': [
               'pyroma = lantz:run_pyroma',
            ],
-           'console_scripts': [
-              'lantz-sim = lantz.simulators:main',
-            ]
-                   },
+        },
       classifiers=[
            'Development Status :: 4 - Beta',
            'Intended Audience :: Developers',
